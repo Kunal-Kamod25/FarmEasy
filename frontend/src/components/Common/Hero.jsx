4a789891084a518790e6b6354f1fc88f46eb1d3c
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-01-12 22:57:14 +0530

    Changes in files

--- frontend/src/components/Common/Hero.jsx
+++ frontend/src/components/Common/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "../Layout/Navbar";
 
 
 const Hero = () =>{
@@ -8,11 +8,11 @@ const Hero = () =>{
         {/* Brand info */}
         <div>
             <div>
-                <h1 className="font-tinos text-2xl text-black text-large font-medium ">Welcome to FarmEasy</h1>
+                <h1 className="text-center font-tinos text-2xl text-black text-large font-medium ">Welcome to FarmEasy</h1>
                 <br/>
-                <h1 className="font-tinos text-2xl text-black text-sm font-medium">Your one-stop Agricultural Marketplace </h1>
+                <h1 className="px-20 font-tinos text-2xl text-black text-sm font-medium">Your one-stop Agricultural Marketplace </h1>
                 <br/>
-                <h1 className="font-tinos text-2xl text-black text-sm font-medium">Browse thousands of products from trusted vendors. Quality equipment, seeds, fertilizers, and irrigation systems for modern farming.</h1>
+                <h1 className="px-20 font-tinos text-2xl text-black text-sm font-medium">Browse thousands of products from trusted vendors. Quality equipment, seeds, fertilizers, and irrigation systems for modern farming.</h1>
             </div>
         </div>
         {/* Hero images */}
---
+++ frontend/src/components/Layout/Thirdbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+const Thirdbar = () => {
+  return (
+    <div className="bg-[#0C970C] text-white">
+      <nav className="container mx-auto flex items-center py-1 px-6">
+        {/* Center - Navigation Links */}
+        <div className="hidden md:flex space-x-4 flex-grow">
+            <Link to="#" className="font-inter text-2xl text-white hover:text-gray-300 text-sm 
+            font-medium uppercase hover:underline cursor-pointer ">Brands</Link>
+            <Link to="#" className="font-inter text-2xl text-white hover:text-gray-300 text-sm 
+            font-medium uppercase hover:underline cursor-pointer ">Fertilizers</Link>
+            <Link to="#" className="font-inter text-2xl text-white hover:text-gray-300 text-sm 
+            font-medium uppercase hover:underline cursor-pointer ">Equipment</Link>
+            <Link to="#" className="font-inter text-2xl text-white hover:text-gray-300 text-sm 
+            font-medium uppercase hover:underline cursor-pointer ">seeds</Link>
+            <Link to="#" className="font-inter text-2xl text-white hover:text-gray-300 text-sm 
+            font-medium uppercase hover:underline cursor-pointer ">irrigation</Link>
+        </div>
+
+      </nav>
+    </div>
+  );
+};
+
+export default Thirdbar;
