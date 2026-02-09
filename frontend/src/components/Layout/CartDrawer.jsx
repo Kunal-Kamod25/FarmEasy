15eb3534157539b3a8605348d072fd30b0533631
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-02-09 19:00:44 +0530

    remove node_modules

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -24,35 +24,35 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
     };
   }, [drawerOpen, toggleCartDrawer]);
 
-    return (
-        <div 
-        ref = {drawerRef}
-        onPointerDown={(e) => e.stopPropagation()}
-        className={'fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ' + (drawerOpen ? 'translate-x-0' : 'translate-x-full')}>
-            
-        {/* Close button */}
-        <div className="flex justify-end p-4">
-            <button onClick={toggleCartDrawer} >
-                <IoMdClose className="h-6 w-6 text-black hover:text-red-600"/>
-            </button>
+  return (
+    <div
+      ref={drawerRef}
+      onPointerDown={(e) => e.stopPropagation()}
+      className={'fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ' + (drawerOpen ? 'translate-x-0' : 'translate-x-full')}>
+
+      {/* Close button */}
+      <div className="flex justify-end p-4">
+        <button onClick={toggleCartDrawer} >
+          <IoMdClose className="h-6 w-6 text-black hover:text-red-600" />
+        </button>
+      </div>
+
+      {/* Cart Content */}
+      <div className="flex-grow p-4 overflow-y-auto scrollbar-hide h-full">
+        <h2 className="text-xl text-black font-semibold mb-4 hover:underline cursor-pointer">Your Cart</h2>
+        <CartContents />
+      </div>
+
+      {/* Checkout Button */}
+      <div className="p-4 border-t sticky border-gray-200">
+        <button className="w-full bg-green-600 text-white py-2 rounded-md hover:bg-green-700 hover:underline cursor-pointer">
+          Proceed to Checkout
+        </button>
+        <div className="p-0.5 pl-1 border-t sticky border-gray-200">
+          <p className="font-inter text-sm tracking-tighter text-black whitespace-nowrap ">Shipping, taxes, and discount codes calculated at checkout.</p>
         </div>
-        
-        {/* Cart Content */}
-        <div className="flex-grow p-4 overflow-y-auto scrollbar-hide h-full">
-            <h2 className="text-xl text-black font-semibold mb-4 hover:underline cursor-pointer">Your Cart</h2>
-            <CartContents />
-        </div>
-        
-        {/* Checkout Button */}
-        <div className="p-4 border-t sticky border-gray-200">
-            <button className="w-full bg-green-600 text-white py-2 rounded-md hover:bg-green-700 hover:underline cursor-pointer">
-                Proceed to Checkout
-            </button>
-            <div className="p-0.5 pl-1 border-t sticky border-gray-200">
-            <p className="font-inter text-sm tracking-tighter text-black whitespace-nowrap ">Shipping, taxes, and discount codes calculated at checkout.</p>
-        </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 };
 export default CartDrawer;
