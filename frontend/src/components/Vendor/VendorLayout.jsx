8432ac749cb7cbb70aad44dab9159bfb81dd2a6a
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-03-27 17:58:51 +0530

    Updated vendor dashobard

--- frontend/src/components/Vendor/VendorLayout.jsx
+++ frontend/src/components/Vendor/VendorLayout.jsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import VendorSidebar from "./VendorSidebar";
+import VendorNavbar from "./VendorNavbar";
 
 const VendorLayout = () => {
     return (
-        <div className="flex min-h-screen bg-gray-50">
-            <VendorSidebar />
-            <div className="flex-1 overflow-auto">
-                <Outlet />
+        <div className="flex flex-col h-screen bg-gray-50 overflow-hidden">
+            {/* 1. Navbar at the top spanning 100% width */}
+            <VendorNavbar />
+            
+            <div className="flex flex-1 overflow-hidden">
+                {/* 2. Sidebar below the navbar, sticky in its own area */}
+                <VendorSidebar />
+                
+                {/* 3. Main content area scrollable independently */}
+                <main className="flex-1 overflow-auto bg-gray-50/50 relative">
+                    <Outlet />
+                </main>
             </div>
         </div>
     );
