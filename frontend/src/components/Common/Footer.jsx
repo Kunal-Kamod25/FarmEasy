44aec1ff2bb72a6c3f5dcae4863d3957257fa989
Kunal <kunalkamod3@gmail.com>
2026-05-08 11:21:33 +0530

    Fix: Added missing useEffect import in Footer.jsx

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import logo from "../../assets/Logo.png";
 import { FiMail, FiPhoneCall } from "react-icons/fi";
